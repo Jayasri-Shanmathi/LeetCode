e0e5af9ddffd48ea36c1251067c66c75a5830360
Jayasri Shanmathi V <jayasrishanmathi@gmail.com>
2026-01-12 22:49:04 +0530

    Time: 55 ms (21.66%) | Memory: 47.9 MB (36.18%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n1=s.length();int n2=p.length();
+        boolean[][]dp=new boolean[n1+1][n2+1];
+        dp[0][0]=true;
+        for(int i=1;i<=n2;i++){
+            if(p.charAt(i-1)=='*')dp[0][i]=dp[0][i-1];
+        }
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(p.charAt(j-1)=='?'|| s.charAt(i-1)==p.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
+                else if(p.charAt(j-1)=='*') dp[i][j]= (dp[i-1][j] || dp[i][j-1]);
+            }
+        }
+        return dp[n1][n2];
+    }
+}
