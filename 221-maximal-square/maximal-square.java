1786b05800db1cf79560843878b42375eb1e04db
Jayasri Shanmathi V <jayasrishanmathi@gmail.com>
2026-06-22 19:29:54 +0530

    Time: 186 ms (5.14%) | Memory: 70.8 MB (73.37%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,19 +1,42 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int r=matrix.length;
-        int c=matrix[0].length;
-        int[][]dp=new int[r+1][c+1];
-        int maxside=0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(matrix[i][j]=='1'){
-                    int min=Math.min(dp[i+1][j],dp[i][j+1]);
-                    dp[i+1][j+1]=1+Math.min(min,dp[i][j]);
-                }
-                maxside=Math.max(maxside,dp[i+1][j+1]);
+        int R=matrix.length;
+        int C=matrix[0].length;
+        int[][]mat=new int[R][C];
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                if(matrix[i][j]=='1') mat[i][j]++;
             }
         }
-        return maxside*maxside;
+        for(int i=0;i<R;i++){
+            for(int j=1;j<C;j++){
+                if(mat[i][j]==0) continue;
+                mat[i][j]+=mat[i][j-1];
+            }
+        }
+        int maxArea=0;
+        for(int j=0;j<C;j++){
+            for(int i=0;i<R;i++){
+                if(mat[i][j]==0) continue;
+                int w=mat[i][j];
 
+                for(int k=i;k<R;k++){
+                    w=Math.min(w,mat[k][j]);
+                    int h=k-i+1;
+                    if(h==w){
+                        maxArea=Math.max(maxArea,h*h);
+                    }
+                }
+
+                for(int k=i;k>=0;k--){
+                    w=Math.min(w,mat[k][j]);
+                    int h=i-k+1;
+                    if(h==w){
+                        maxArea=Math.max(maxArea,h*h);
+                    }
+                }
+            }
+        }
+        return maxArea;
     }
 }
